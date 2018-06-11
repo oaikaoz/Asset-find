09c98176b5e5df5c1dd59591e09720c69ab18bd8
oaikaoz <oaikaoz@hotmail.com>
2018-06-11 16:59:45 +0700

    test

--- getContent.php
+++ getContent.php
@@ -1,5 +1,5 @@
 <?php
-$homepage = file_get_contents('http://10.11.15.126/asset_ecs/uploadImage/getContent.php?tag=BBD1799688');
+$homepage = file_get_contents('http://androidserver.ddns.net/findAsset/getContent.php?tag=BBD1799688');
 echo $homepage;
 
 ?>
