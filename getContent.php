279242c968385b1a9c4048cb2b6175fccd2b5db2
oaikaoz <oaikaoz@hotmail.com>
2018-06-11 17:45:36 +0700

    test

--- getContent.php
+++ getContent.php
@@ -1,7 +1,8 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 //$server = $_SERVER['SERVER_NAME'];
-//$data = file_get_contents('http://'.$server.'/findAsset/getContent.php?tag=BBD1799688');
-echo 'test';
+$server ='183.89.76.57:85';
+$data = file_get_contents('http://'.$server.'/findAsset/getContent.php?tag=BBD1799688');
+echo $data;
 
 ?>
