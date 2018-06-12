8739b42c529ebe5e790ed8474b2eb746565eb96a
oaikaoz <oaikaoz@hotmail.com>
2018-06-12 08:09:52 +0700

    edit Get Tag line 6

--- getContent.php
+++ getContent.php
@@ -1,8 +1,9 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
+$tag = $_GET['tag'];
 //$server = $_SERVER['SERVER_NAME'];
 $server ='183.89.76.57:85';
-$data = file_get_contents('http://'.$server.'/findAsset/getContent.php?tag=BBD1799688');
+$data = file_get_contents('http://'.$server.'/findAsset/getContent.php?tag='.$tag);
 echo $data;
 
 ?>
