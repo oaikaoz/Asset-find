095a9eab9531b7852559fb21f13571aa5b56d4bb
oaikaoz <oaikaoz@hotmail.com>
2018-06-12 10:25:51 +0700

    edit data

--- getContent.php
+++ getContent.php
@@ -4,8 +4,6 @@ $tag = $_GET['tag'];
 //$server = $_SERVER['SERVER_NAME'];
 $server ='183.89.76.57:85';
 $data = file_get_contents('http://'.$server.'/findAsset/getContent.php?tag='.$tag);
-//echo $data;
-$ip = $_SERVER['REMOTE_ADDR'];
-echo $ip;
-echo $ip;
+echo $data;
+
 ?>
