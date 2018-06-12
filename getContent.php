d959a86f63336fd864209ae9a9f28e6c89208a24
oaikaoz <oaikaoz@hotmail.com>
2018-06-12 10:32:42 +0700

    edit datas..

--- getContent.php
+++ getContent.php
@@ -4,8 +4,8 @@ $tag = $_GET['tag'];
 //$server = $_SERVER['SERVER_NAME'];
 $server ='183.89.76.57:85';
 $data = file_get_contents('http://'.$server.'/findAsset/getContent.php?tag='.$tag);
-//echo $data;
-$ip = gethostbyname(parse_url('http://androidserver.ddns.net/', PHP_URL_HOST));
-echo $ip;
+echo $data;
+//$ip = gethostbyname(parse_url('http://androidserver.ddns.net/', PHP_URL_HOST));
+//echo $ip;
 
 ?>
