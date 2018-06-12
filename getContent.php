b5acd805490555b377cfffe44edfe755975bb967
oaikaoz <oaikaoz@hotmail.com>
2018-06-12 11:42:51 +0700

    edit data

--- getContent.php
+++ getContent.php
@@ -5,5 +5,6 @@
     //$server ='183.89.76.57:85';
     $server ='androidserver.ddns.net';
     $data = file_get_contents('http://'.$server.'/findAsset/getContent.php?tag='.$tag);
-    echo $data;
+    
+    echo $data;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
 ?>
