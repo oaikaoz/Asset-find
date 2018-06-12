1d0887339ef24752f52108a9747faa1bd5079674
oaikaoz <oaikaoz@hotmail.com>
2018-06-12 11:39:34 +0700

    confirm

--- getContent.php
+++ getContent.php
@@ -1,11 +1,9 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
-$tag = $_GET['tag'];
-//$server = $_SERVER['SERVER_NAME'];
-//$server ='183.89.76.57:85';
-$server ='androidserver.ddns.net';
-$data = file_get_contents('http://'.$server.'/findAsset/getContent.php?tag='.$tag);
-echo $data;
-
-
+    header('Content-Type: text/html; charset=utf-8');
+    $tag = $_GET['tag'];
+    //$server = $_SERVER['SERVER_NAME'];
+    //$server ='183.89.76.57:85';
+    $server ='androidserver.ddns.net';
+    $data = file_get_contents('http://'.$server.'/findAsset/getContent.php?tag='.$tag);
+    echo $data;
 ?>
