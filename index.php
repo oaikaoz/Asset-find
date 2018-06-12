46653d4e5db7450eee83afda317f6a29b547c5d6
oaikaoz <oaikaoz@hotmail.com>
2018-06-12 08:14:19 +0700

    add loading Css

--- index.php
+++ index.php
@@ -26,12 +26,34 @@
     
       color:#cc5128;
       }
+	  .loader {
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid blue;
+  border-right: 16px solid green;
+  border-bottom: 16px solid red;
+  width: 120px;
+  height: 120px;
+  -webkit-animation: spin 2s linear infinite;
+  animation: spin 2s linear infinite;
+}
+
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
 </style>
 <script type="text/javascript">
 
         function getData(tag){
           
-          //document.getElementById("data").innerHTML = "<center><div class='loader'></div></center>";                                    
+          document.getElementById("data").innerHTML = "<center><div class='loader'></div></center>";                                    
            
             $.get("getContent.php?tag="+tag,	
                             function(data,status){
