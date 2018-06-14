53903de24dbc771b1b809c4e36ec2cd4960d1a57
oaikaoz <oaikaoz@hotmail.com>
2018-06-14 15:20:07 +0700

    add fab button test

--- index.php
+++ index.php
@@ -59,6 +59,39 @@
 			}
 
 
+
+
+
+
+
+
+.fab {
+   width: 70px;
+   height: 70px;
+   background-color: red;
+   border-radius: 50%;
+   box-shadow: 0 6px 10px 0 #666;
+   
+   font-size: 50px;
+   line-height: 70px;
+   color: white;
+   text-align: center;
+   
+   position: fixed;
+   right: 50px;
+   bottom: 50px;
+   
+  transition: all 0.1s ease-in-out;
+}
+
+.fab:hover {
+   box-shadow: 0 6px 14px 0 #666;
+   transform: scale(1.05);
+}
+
+
+
+
 </style>
 <script type="text/javascript">
 
@@ -244,6 +277,9 @@ $(function() {
 
 </div>
 
+
+
+<div class="fab"> + </div>
 </body>
 </html>
 
